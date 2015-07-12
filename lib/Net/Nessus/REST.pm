c331770b164f02fabb348cf28610913d1d7edf2e
Guillaume Rousse <guillomovitch@gmail.com>
2015-07-12 17:18:34 +0200

    add an additional wait loop in documentation

--- lib/Net/Nessus/REST.pm
+++ lib/Net/Nessus/REST.pm
@@ -494,6 +494,10 @@ using REST interface.
         format  => 'pdf'
     );
 
+    while ($nessus->get_scan_export_status(scan_id => $scan_id, file_id => $file_id) ne 'ready') {
+        sleep 1;
+    }
+
     $nessus->download_report(
         scan_id  => $scan->{id},
         file_id  => $file_id,
